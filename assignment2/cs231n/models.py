b55545c7c9cdf89ca7bef8fabacf6d604f7fa802
Michal <michal@mdevel.net>
2018-02-21 10:49:28 +0100

    added two more conv relu pool models

--- assignment2/cs231n/models.py
+++ assignment2/cs231n/models.py
@@ -58,3 +58,70 @@ conv_relu_pool_model = nn.Sequential(
     nn.Linear(in_features=500, out_features=10)
 )
 CONV_RELU_POOL = ("ConvReluPool", conv_relu_pool_model)
+
+
+conv_relu_pool_2_model = nn.Sequential(
+    nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, stride=1, padding=2),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    # 16x16x64
+
+    nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    # 8x8x128
+
+    nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    # 4x4x256
+
+    Flatten(),
+
+    nn.Linear(in_features=4096, out_features=2048),
+    nn.ReLU(inplace=True),
+
+    nn.Linear(in_features=2048, out_features=1024),
+    nn.ReLU(inplace=True),
+
+    nn.Linear(in_features=1024, out_features=10)
+)
+CONV_RELU_POOL_2 = ("ConvReluPool2", conv_relu_pool_model)
+
+conv_relu_pool_3_model = nn.Sequential(
+    nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    # 16x16x64
+
+    nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    # 8x8x128
+
+    nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    # 4x4x256
+
+    nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    # 2x2x512
+
+    nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, stride=1, padding=1),
+    nn.ReLU(inplace=True),
+    nn.MaxPool2d(kernel_size=2, stride=2),
+    # 1x1x1024
+
+    Flatten(),
+
+    nn.Linear(in_features=1024, out_features=1024),
+    nn.ReLU(inplace=True),
+
+    nn.Linear(in_features=1024, out_features=1024),
+    nn.ReLU(inplace=True),
+
+    nn.Linear(in_features=1024, out_features=10)
+)
+CONV_RELU_POOL_3 = ("ConvReluPool3", conv_relu_pool_model)
