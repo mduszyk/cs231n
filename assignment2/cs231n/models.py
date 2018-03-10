01ff2dd13c9fb70661613c0aa700d8575ce01f7b
Michal <michal@mdevel.net>
2018-03-10 09:53:21 +0100

    dropout for conv conv pool

--- assignment2/cs231n/models.py
+++ assignment2/cs231n/models.py
@@ -366,38 +366,47 @@ def conv_conv_pool_2_dr(dropout=0.5):
     return nn.Sequential(
         nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.MaxPool2d(kernel_size=2, stride=2),
         # 16x16x64
 
         nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.MaxPool2d(kernel_size=2, stride=2),
         # 8x8x128
 
         nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.MaxPool2d(kernel_size=2, stride=2),
         # 4x4x256
 
         nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
         nn.ReLU(inplace=True),
+        nn.Dropout2d(p=dropout, inplace=True),
         nn.MaxPool2d(kernel_size=2, stride=2),
         # 2x2x512
 
         Flatten(),
-        nn.Dropout(p=dropout, inplace=True),
 
         nn.Linear(in_features=2048, out_features=1024),
         nn.ReLU(inplace=True),
